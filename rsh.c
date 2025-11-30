2099819266e4ed71141e6a24d62079581db06c31
aidenmurphy-a11y <aiden_murphy@mines.edu>
2025-11-30 02:05:16 -0500

    Refactor isAllowed function and improve command handling

--- rsh.c
+++ rsh.c
@@ -6,88 +6,110 @@
 #include <string.h>
 
 #define N 12
+#define MAXARGS 21   // 20 args + NULL
 
 extern char **environ;
 
 char *allowed[N] = {"cp","touch","mkdir","ls","pwd","cat","grep","chmod","diff","cd","exit","help"};
 
-int isAllowed(const char*cmd) {
-	// TODO
-	// return 1 if cmd is one of the allowed commands
-	// return 0 otherwise
-	for(int i = 0; i < N; i++){
-		if (strcmp(cmd, allowed[i]) == 0){
-			return 1;
-		}
-	return 0;
+int isAllowed(const char* cmd) {
+
+    for (int i = 0; i < N; i++) {
+        if (strcmp(cmd, allowed[i]) == 0) {
+            return 1;
+        }
+    }
+
+    return 0;
 }
 
 int main() {
-
-    // TODO
-    // Add variables as needed
 
     char line[256];
 
     while (1) {
 
-		fprintf(stderr,"rsh>");
+        fprintf(stderr, "rsh>");
 
-		if (fgets(line,256,stdin)==NULL) continue;
+        if (fgets(line, 256, stdin) == NULL) continue;
 
-		if (strcmp(line,"\n")==0) continue;
+        if (strcmp(line, "\n") == 0) continue;
 
-		line[strlen(line)-1]='\0';
+        line[strlen(line)-1] = '\0';
 
-	// TODO
-	// Add code to spawn processes for the first 9 commands
-	// And add code to execute cd, exit, help commands
-	// Use the example provided in myspawn.c
+        // -----------------------------
+        // Build argv
+        // -----------------------------
+        char *argv[MAXARGS];
+        int argc = 0;
 
-		char *argv[MAXARGS];
-		int argc = 0;
-		char *tok = strtok(line, " ");
-		while (tok != NULL && argc < MAXARGS -1){
-			argv[argc++] = token;
-		}
-		argv[argc] = NULL;
-		if (argc == 0){
-			continue;
-		}
-		char *cmd = argv[0];
-		if(!isAllowed(cmd)){
-			printf("NOT ALLOWED!\n");
-			continue;
-		}
+        char *tok = strtok(line, " ");
 
-		if (strcmp(cmd, "exit") == 0){
-			return 0;
-		}
+        while (tok != NULL && argc < MAXARGS - 1) {
+            argv[argc++] = tok;
+            tok = strtok(NULL, " ");
+        }
 
-		if(strcmp(cmd, "help")==0){
-			printf("The allowed commands are:\n");
-			for (int i = 0; i < N; i++){
-				printf("%d: %s\n", i+1, allowed[i]);
-			}
-			continue;
-		}
+        argv[argc] = NULL;
 
-		if(strcmp(cmd, "cd") == 0){
-			if (argc > 2){
-				printf("-rsh: cd: too many arguments\n");
-				continue;
-			}
-			char *targ = (argc == 1) ? getenv("HOME") : argv[1];
-			chdir(targ);
-			continue;
-		}
-		pid_t pid;
-		int stat;
-		if (posix_spawnp(&pid, cmd, NULL, NULL, argv, environ) != 0){
-			printf("NOT ALLOWED!\n");
-			continue;
-		}
-		wait(pid, &stat, 0);
+        if (argc == 0) continue;
+
+        char *cmd = argv[0];
+
+        // -----------------------------
+        // Check allowed
+        // -----------------------------
+        if (!isAllowed(cmd)) {
+            printf("NOT ALLOWED!\n");
+            continue;
+        }
+
+        // -----------------------------
+        // exit
+        // -----------------------------
+        if (strcmp(cmd, "exit") == 0) {
+            return 0;
+        }
+
+        // -----------------------------
+        // help
+        // -----------------------------
+        if (strcmp(cmd, "help") == 0) {
+            printf("The allowed commands are:\n");
+            for (int i = 0; i < N; i++) {
+                printf("%d: %s\n", i+1, allowed[i]);
+            }
+            continue;
+        }
+
+        // -----------------------------
+        // cd
+        // -----------------------------
+        if (strcmp(cmd, "cd") == 0) {
+
+            if (argc > 2) {
+                printf("-rsh: cd: too many arguments\n");
+                continue;
+            }
+
+            char *targ = (argc == 1) ? getenv("HOME") : argv[1];
+            chdir(targ);
+            continue;
+        }
+
+        // -----------------------------
+        // spawn
+        // -----------------------------
+        pid_t pid;
+        int stat;
+
+        if (posix_spawnp(&pid, cmd, NULL, NULL, argv, environ) != 0) {
+            printf("NOT ALLOWED!\n");
+            continue;
+        }
+
+        waitpid(pid, &stat, 0);
     }
+
     return 0;
 }
