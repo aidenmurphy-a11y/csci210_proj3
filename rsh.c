259dc06945cd536dc7e2350ff9d9498368a540eb
aidenmurphy-a11y <aiden_murphy@mines.edu>
2025-11-29 20:27:55 -0500

    Correct command validation and process spawning logic
    Fix syntax errors and implement command handling.

--- rsh.c
+++ rsh.c
@@ -15,7 +15,10 @@ int isAllowed(const char*cmd) {
 	// TODO
 	// return 1 if cmd is one of the allowed commands
 	// return 0 otherwise
-	
+	fo(int i = 0; i < N; i++){
+		if (strcmp(cmd, allowed[i]) == 0){
+			return 1;
+		}
 	return 0;
 }
 
@@ -28,19 +31,63 @@ int main() {
 
     while (1) {
 
-	fprintf(stderr,"rsh>");
+		fprintf(stderr,"rsh>");
 
-	if (fgets(line,256,stdin)==NULL) continue;
+		if (fgets(line,256,stdin)==NULL) continue;
 
-	if (strcmp(line,"\n")==0) continue;
+		if (strcmp(line,"\n")==0) continue;
 
-	line[strlen(line)-1]='\0';
+		line[strlen(line)-1]='\0';
 
 	// TODO
 	// Add code to spawn processes for the first 9 commands
 	// And add code to execute cd, exit, help commands
 	// Use the example provided in myspawn.c
 
+		char *argv[MAXARGD];
+		int argc = 0;
+		char *tok = strtok(line, " ");
+		while (tok != NULL && argc < MAXARGS -1){
+			argv[argc++] = token;
+		}
+		argv[argc] = NULL;
+		if (argc == 0){
+			continue;
+		}
+		char *cmd = argv[0];
+		if(!isAllowed(cmd)){
+			printf("NOT ALLOWED!\n")
+				continue;
+		}
+
+		if (strcmp(cmd, "exit") == 0){
+			return 0;
+		}
+
+		if(strcmp(cmd, "help")==0){
+			printf("The allowed commands are:\n");
+			for (int i = 0: i < N; i++){
+				printf("%d: %s\n", i+1, allowed[i]);
+			}
+			continue;
+		}
+
+		if(strcmp(cmd, "cd") == 0){
+			if (argc > 2){
+				printf("-rsh: cd: too many arguments\n");
+				continue;
+			}
+			char *targ = (argc == 1) ? getenv("HOME") : argv[1];
+			chdir(targ);
+			continue;
+		}
+		pid_t pid;
+		int stat;
+		if (posix_spawnp(&pid, cmd, NULL, NULL, argv, environ) != 0){
+			printf("NOT ALLOWED!\n")
+				continue;
+		}
+		wait(pid, &stat, 0);
     }
     return 0;
 }
